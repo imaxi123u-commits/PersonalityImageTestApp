f4a8ee238b137e81a63d008c7ff42e4afdb21a96
Sufiyan Maqsood <hefep39345@cexch.com>
2026-05-14 16:49:04 +0500

    Add project files.

---
+++ PersonalityImageTestApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalityImageTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Result(string choice)
+        {
+            string personality;
+
+            switch (choice)
+            {
+                case "nature":
+                    personality = "Calm and Peaceful Personality";
+                    break;
+
+                case "city":
+                    personality = "Ambitious and Energetic Personality";
+                    break;
+
+                case "space":
+                    personality = "Creative and Curious Personality";
+                    break;
+
+                default:
+                    personality = "Unknown Personality";
+                    break;
+            }
+
+            ViewBag.Result = personality;
+            return View();
+        }
+    }
+}
---
+++ PersonalityImageTestApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalityImageTestApp.MvcApplication" Language="C#" %>
